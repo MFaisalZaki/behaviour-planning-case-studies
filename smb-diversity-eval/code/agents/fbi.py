cc0598742de408eb637049c820be69bb5e421266
Mustafa Faisal <mustafafaisal@Mustafas-MacBook-Pro-3.local>
2025-08-29 01:19:41 +0100

    initial fbi operation for smb.

--- smb-diversity-eval/code/agents/fbi.py
+++ smb-diversity-eval/code/agents/fbi.py
@@ -1,18 +1,23 @@
 from pcg_benchmark.probs.smbtile.engine.core import MarioWorld, MarioActions, MarioForwardModel
 from pcg_benchmark.probs.smbtile.engine.agents.astar import AStarTree, TreeNode, GameStatus, getActionString
-
-# We need to implement an FBI agent.
+from flloat.parser.ltlf import LTLfParser
 
 class SuperMarioPlan:
-    def __init__(self, actions, behaviour):
+    def __init__(self, actions, behaviour, planltl):
         self.actions = actions
         self.behaviour = behaviour
+        self.planltl = planltl
 
-# I would say the diversity would be:
-# how much coins collected and how many enemies are killed.
+class BehaviourCount:
+    def __init__(self, dims):
+        self.dimensions = dims
+    def count(self, plans):
+        return len(set(p.behaviour for p in plans))
+
 class SuperMarioDiversityDim:
     def __init__(self, name):
         self.name = name
+        self.domain = set()
     
     def abstract(self, state_trace, actions_list):
         assert False, "This should be implemented by the feature"
@@ -24,17 +29,21 @@ class SuperMarioPlanDim(SuperMarioDiversityDim):
     def __init__(self):
         super().__init__("plan")
 
-    def abstract(self, state_trace, actions_list):
+    def __actions_representation__(self, plan_actions):
         action_string = {}
-        for t, action in enumerate(actions_list):
+        for t, action in enumerate(plan_actions):
             for it, a in enumerate(action):
                 actionname = getActionString(a).strip().lower().replace(' ', '_')
                 action_string[f't{t}_it{it}_{actionname}'] = True
-        return action_string | {f'{self.name}_goal_state': state_trace[-1].gameStatus == GameStatus.WIN}
+        return action_string
+
+    def abstract(self, state_trace, actions_list):
+        return self.__actions_representation__(actions_list) | {f'{self.name}_goal_state': state_trace[-1].gameStatus == GameStatus.WIN}
     
     def extract_behaviour(self, state_trace, actions_list):
-        
-        assert False, "This should be implemented by the feature"
+        ret_behaviour = self.__actions_representation__(actions_list)
+        self.domain.add(' & '.join(ret_behaviour.keys()))
+        return ret_behaviour
 
 class SuperMarioCoinDim(SuperMarioDiversityDim):
     def __init__(self):
@@ -47,6 +56,7 @@ class SuperMarioCoinDim(SuperMarioDiversityDim):
         ltl_trace = self.abstract(state_trace, [])
         key = list(filter(lambda k:  f'{len(state_trace)-1}_{self.name}' in k, ltl_trace.keys()))[0]
         ret_behaviour_ltl = f"FG({key} & {self.name}_goal_state)"
+        self.domain.add(ret_behaviour_ltl)
         return ret_behaviour_ltl
 
 class SuperMarioEnemyDim(SuperMarioDiversityDim):
@@ -61,6 +71,7 @@ class SuperMarioEnemyDim(SuperMarioDiversityDim):
         ltl_trace = self.abstract(state_trace, [])
         key = list(filter(lambda k:  f'{len(state_trace)-1}_{self.name}' in k, ltl_trace.keys()))[0]
         ret_behaviour_ltl = f"FG({key} & {self.name}_goal_state)"
+        self.domain.add(ret_behaviour_ltl)
         return ret_behaviour_ltl
 
 class SuperMarioBehaviourSpace:
@@ -81,13 +92,22 @@ class SuperMarioBehaviourSpace:
     def infer(self, state):
         state_trace, actionslist = self.__state_trace__(state)
         dims_behaviour = [dim.extract_behaviour(state_trace, actionslist) for dim in filter(lambda d: not isinstance(d, SuperMarioPlanDim), self.dimensions)]
+        return f"({' & '.join(dims_behaviour)})"
 
-        pass
+    def represent_plan(self, state):
+        state_trace, actionslist = self.__state_trace__(state)
+        dims_behaviour = [dim.extract_behaviour(state_trace, actionslist) for dim in filter(lambda d: isinstance(d, SuperMarioPlanDim), self.dimensions)]
+        return f"({' & '.join(dims_behaviour[0].keys())})"
 
     def check_behaviour(self, state, behaviours):
+        if len(behaviours) == 0: return False
         state_trace, actionslist = self.__state_trace__(state)
         ltl_trace = [dim.abstract(state_trace, actionslist) for dim in self.dimensions]
         ltl_trace = {k:v for d in ltl_trace for k,v in d.items()}
+        
+        if any([b.truth([ltl_trace]) for b in behaviours]):
+            return True
+
         return False
 
 class SuperMarioFBIAgent(AStarTree):
@@ -108,21 +128,22 @@ class SuperMarioFBIAgent(AStarTree):
         MarioForwardModel.maxMoves = 100
         return MarioForwardModel(_world)
 
-    def search(self, k, level, maxIterations=100, timer=20, behaviours=[]):
+    def search(self, k, level, maxIterations=100, timer=20, plans=[], forbid_behaviours=False):
         model = self.__construct_world_model__(level, timer)
         root = TreeNode(None, self._repetitions, model.clone(), None)
         visitedStates = []
         queue = root.generateChildren()
         bspace = SuperMarioBehaviourSpace(self.dimensions, model.clone())
+        ltl_parser = LTLfParser()
+        behaviours = [ltl_parser(f'({p.behaviour})')  for p in plans]
         while len(queue) > 0 and maxIterations > 0 and len(self.solutions) < k:
             queue = sorted(queue, key=lambda v: v.getHeuristic() - 0.9 * v.getCost(), reverse=True)
             current = queue.pop(0)
             if self.isInVisited(visitedStates, current.getKey()): continue
-            if bspace.check_behaviour(current, behaviours): continue
+            if forbid_behaviours and bspace.check_behaviour(current, behaviours): continue
             visitedStates.append(current.getKey())
             if current._model.getGameStatus() == GameStatus.WIN: 
-                self.solutions.append(SuperMarioPlan(current.getNextActions(), bspace.infer(current)))
-                bspace.check_behaviour(current, behaviours)
+                self.solutions.append(SuperMarioPlan(current.getNextActions(), bspace.infer(current), bspace.represent_plan(current)))
             queue = queue + current.generateChildren()
             maxIterations -= 1
         return self.solutions
--- smb-diversity-eval/code/main.py
+++ smb-diversity-eval/code/main.py
@@ -3,15 +3,27 @@ from pcg_benchmark.probs.smbtile.engine.core import MarioGame
 from pcg_benchmark.probs.smbtile.engine.agents.astar import Agent
 import pcg_benchmark
 
-from agents.fbi import SuperMarioFBIAgent, SuperMarioCoinDim, SuperMarioEnemyDim
+from agents.fbi import BehaviourCount, SuperMarioFBIAgent, SuperMarioCoinDim, SuperMarioEnemyDim
 
 env = pcg_benchmark.make("smbtile-v0")
 
 levelString = open('/Users/mustafafaisal/Developer/behaviour-planning-case-studies/smb-diversity-eval/venv/lib/python3.12/site-packages/pcg_benchmark/probs/smb/slices.txt').read()
 
 dims = [SuperMarioCoinDim(), SuperMarioEnemyDim()]
-fbiagent = SuperMarioFBIAgent(dims)
-plans = fbiagent.search(2, levelString)
+fbiagent_naive = SuperMarioFBIAgent(dims)
+plans = fbiagent_naive.search(5, levelString)
+
+bc_count = BehaviourCount(dims).count(plans)
+
+pass
+diverse_plans = []
+while len(diverse_plans) < 5:
+    fbiagent_diverse = SuperMarioFBIAgent(dims)
+    plans = fbiagent_diverse.search(1, levelString, plans=diverse_plans, forbid_behaviours=True)
+    if len(plans) == 0: break
+    diverse_plans.extend(plans)
+
+
 
 MarioAgent.iterations = 100
 MarioAgent.stickyActions = 8
