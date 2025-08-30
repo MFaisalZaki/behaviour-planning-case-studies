29adaeb1143e6a7f34c0fb2fa129c078a2fb544c
Mustafa Faisal <mustafafaisal@Mustafas-MacBook-Pro-3.local>
2025-08-30 13:34:35 +0100

    overloading mario actions list.

--- smb-diversity-eval-gb/code/env_case_study.py
+++ smb-diversity-eval-gb/code/env_case_study.py
@@ -9,9 +9,9 @@ class SuperMarioCaseStudy(SuperMarioEnv):
         super().__init__(romfile, render)
         # overload the action space.
         # action_list  = list()
-        # action_list += list(chain.from_iterable([[f'{a},{t}' for a in ['a', 'a+left', 'a+right']] for t in [6, 15]]))
+        action_list += list(chain.from_iterable([[f'{a},{t}' for a in ['a', 'a+left', 'a+right']] for t in [3, 10, 15]]))
         # action_list += list(chain.from_iterable([[f'{a},{t}' for a in ['nop']] for t in [3]]))
-        # action_list += list(chain.from_iterable([[f'{a},{t}' for a in ['left', 'right']] for t in [3]]))
+        action_list += list(chain.from_iterable([[f'{a},{t}' for a in ['left', 'right']] for t in [3]]))
         # self.actions = action_list
 
     def is_goal(self, state):
