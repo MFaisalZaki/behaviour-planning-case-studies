9dbd040be3ebc384c4e13a52abc7b86434b279c7
Mustafa Faisal <mustafafaisal@Mustafas-MacBook-Pro-3.local>
2025-08-30 14:04:41 +0100

    increasing the action space.

--- smb-diversity-eval-gb/code/env_case_study.py
+++ smb-diversity-eval-gb/code/env_case_study.py
@@ -9,10 +9,10 @@ class SuperMarioCaseStudy(SuperMarioEnv):
         super().__init__(romfile, render)
         # overload the action space.
         action_list  = list()
-        action_list += list(chain.from_iterable([[f'{a},{t}' for a in ['a+left', 'a+right']] for t in [10, 15]]))
-        # action_list += list(chain.from_iterable([[f'{a},{t}' for a in ['a', 'a+left', 'a+right']] for t in [3, 10, 15]]))
+        # action_list += list(chain.from_iterable([[f'{a},{t}' for a in ['a+left', 'a+right']] for t in [10, 15]]))
+        action_list += list(chain.from_iterable([[f'{a},{t}' for a in ['a', 'a+left', 'a+right', 'b+left', 'b+right']] for t in [3, 10, 15]]))
         # action_list += list(chain.from_iterable([[f'{a},{t}' for a in ['nop']] for t in [3]]))
-        # action_list += list(chain.from_iterable([[f'{a},{t}' for a in ['left', 'right']] for t in [3]]))
+        action_list += list(chain.from_iterable([[f'{a},{t}' for a in ['left', 'right']] for t in [3]]))
         self.actions = action_list
 
     def is_goal(self, state):
