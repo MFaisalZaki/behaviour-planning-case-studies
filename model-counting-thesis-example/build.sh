1e3e587b422fbb9b8faf4cb7702b75f1db2be158
Mustafa Faisal <mustafafaisal@8afbf073.st-andrews.ac.uk>
2025-11-07 14:43:27 +0000

    adding fi build script.

--- model-counting-thesis-example/build.sh
+++ model-counting-thesis-example/build.sh
@@ -6,4 +6,5 @@ pip install git+https://github.com/MFaisalZaki/pyBehaviourPlanningASP.git
 pip install git+https://github.com/MFaisalZaki/pyBehaviourPlanningSMT.git
 pip uninstall pypmt
 pip install git+https://github.com/pyPMT/pyPMT.git@d44efb71746b3a91e7fb1926b4405bd14f9df33b
+pip install git+https://github.com/MFaisalZaki/forbiditerative.git
 deactivate
